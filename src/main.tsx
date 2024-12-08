1f0961d2fcf5f736fbd82f06f24501e6c1b937e9
Sayam Alvi <sayamalvi07@gmail.com>
2024-12-08 12:56:58 +0530

    Sign in form ui

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
+import 'antd/dist/reset.css'
+import { ConfigProvider } from 'antd'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ConfigProvider theme={{
+      token: {
+        colorPrimary: '#F65F42',
+        colorLink: '#F65F42'
+      }
+    }}>
+      <RouterProvider router={router} />
+    </ConfigProvider>
   </StrictMode>,
 )
