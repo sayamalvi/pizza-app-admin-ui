d192b810573f9078c99600d3d9388ef9a8af4677
Sayam Alvi <sayamalvi07@gmail.com>
2024-12-09 23:05:09 +0530

    add auto refresh tokens

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,14 @@ import 'antd/dist/reset.css'
 import { ConfigProvider } from 'antd'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-const queryClient = new QueryClient()
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
