ce2c8821905cbc04ffa33c007965125cb44aa179
Sayam Alvi <sayamalvi07@gmail.com>
2024-12-09 10:37:22 +0530

    add zustand & user state

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -4,6 +4,7 @@ import Logo from "../../components/icons/Logo"
 import { useMutation, useQuery } from "@tanstack/react-query"
 import { Credentials } from "../../types"
 import { login, self } from "../../http/api"
+import { useAuthStore } from "../../store"
 
 const loginUser = async (credentials: Credentials) => {
     const { data } = await login(credentials)
@@ -14,8 +15,9 @@ const getSelf = async () => {
     return data;
 }
 const LoginPage = () => {
+    const { setUser } = useAuthStore()
 
-    const { data: selfData, refetch } = useQuery({
+    const { refetch } = useQuery({
         queryKey: ['self'],
         queryFn: getSelf,
         enabled: false
@@ -25,8 +27,8 @@ const LoginPage = () => {
         mutationKey: ['login'],
         mutationFn: loginUser,
         onSuccess: async () => {
-            refetch()
-            console.log(selfData)
+            const selfDataPromise = await refetch()
+            setUser(selfDataPromise.data)
         }
     })
 
