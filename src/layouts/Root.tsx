9fe92831ba6a8c5f15e0fff905bbb6f9d49d89e1
Sayam Alvi <sayamalvi07@gmail.com>
2024-12-09 14:39:35 +0530

    maintain auth state on refresh

---
+++ src/layouts/Root.tsx
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query"
+import { self } from "../http/api";
+import { useAuthStore } from "../store";
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+
+const getSelf = async () => {
+    const { data } = await self()
+    return data;
+}
+const Root = () => {
+    const { setUser } = useAuthStore()
+    const { data, isLoading } = useQuery({
+        queryKey: ['self'],
+        queryFn: getSelf,
+    })
+
+    useEffect(() => {
+        if (data) {
+            setUser(data)
+        }
+    }, [data, setUser])
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    return (
+        <div>
+            <h1>Root</h1>
+            <Outlet />
+        </div>
+    )
+}
+
+export default Root
