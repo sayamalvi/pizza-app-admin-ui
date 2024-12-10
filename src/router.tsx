b9cefa27bcefd87c6eba88cb57f379dc0bc8a13f
Sayam Alvi <sayamalvi07@gmail.com>
2024-12-10 23:03:35 +0530

    show users with table

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/login/login";
+import LoginPage from "./pages/login/Login";
 import Dashboard from "./layouts/Dashboard";
 import NonAuth from "./layouts/NonAuth";
 import Root from "./layouts/Root";
+import Users from "./pages/users/Users";
 
 export const router = createBrowserRouter([
     {
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
                     {
                         path: '',
                         element: <HomePage />
+                    },
+                    {
+                        path: 'users',
+                        element: <Users />
                     },
                 ]
             },
