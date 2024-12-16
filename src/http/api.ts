8174d3799506ddb21803e774ee5d6a32e505284b
Sayam Alvi <sayamalvi07@gmail.com>
2024-12-16 15:01:10 +0530

    add api gateway

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,34 +1,39 @@
 import { CreateUserData, Credentials, UpdateUserData } from "../types";
 import { api } from "./client";
 
+export const AUTH_SERVICE = "/api/auth";
+export const CATALOG_SERVICE = '/api/catalog';
+
 export const login = (credentials: Credentials) => {
-  return api.post("/auth/login", credentials);
+  return api.post(`${AUTH_SERVICE}/auth/login`, credentials);
 };
 
 export const self = () => {
-  return api.get("/auth/self");
+  return api.get(`${AUTH_SERVICE}/auth/self`);
 };
 
 export const logout = () => {
-  return api.post("/auth/logout");
+  return api.post(`${AUTH_SERVICE}/auth/logout`);
 };
 
 export const getUsers = (queryString: string) => {
-  return api.get(`/users?${queryString}`);
+  return api.get(`${AUTH_SERVICE}/users?${queryString}`);
 };
 
 export const getTenants = () => {
-  return api.get("/tenants");
+  return api.get(`${AUTH_SERVICE}/tenants`);
 };
 
 export const createUser = (user: CreateUserData) => {
-  return api.post("/users", user);
+  return api.post(`${AUTH_SERVICE}/users`, user);
 };
 
 export const updateUser = (id: string, user: UpdateUserData) => {
-  return api.patch(`/users/${id}`, user);
+  return api.patch(`${AUTH_SERVICE}/users/${id}`, user);
 };
 
 export const deleteUser = (id: string) => {
-  return api.delete(`/users/${id}`);
+  return api.delete(`${AUTH_SERVICE}/users/${id}`);
 };
+
+
